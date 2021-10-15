d530908802b31660a0920ef30d2b8dd149d7fa5f
Jean-Carlos-A-Souza <jean.carlos.alb.souza@gmail.com>
2021-10-15 15:18:56 -0300

    Implementação do Room DataBase

--- app/src/main/java/com/apolo/businesscard/App.kt
+++ app/src/main/java/com/apolo/businesscard/App.kt
@@ -1,6 +1,10 @@
 package com.apolo.businesscard
 
 import android.app.Application
+import com.apolo.businesscard.data.AppDataBase
+import com.apolo.businesscard.data.BusinessCardRepositoy
 
 class App : Application() {
+    val dataBase by lazy { AppDataBase.getDataBase( this) }
+    val repositoy by lazy { BusinessCardRepositoy(dataBase.businessDao()) }
 }
---
+++ app/src/main/java/com/apolo/businesscard/data/BusinessCard.kt
@@ -0,0 +1,13 @@
+package com.apolo.businesscard.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class BusinessCard (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nome: String,
+    val empresa: String,
+    val email: String,
+    val fundoPersonalizado: String
+)
